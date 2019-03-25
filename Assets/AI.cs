f2c9f7f8cc1bdb2c79c9fb829aea5f8378b62571
Duy <etn218n@gmail.com>
2019-03-25 11:16:03 +0200

    Minor Adjustment

--- Assets/AI.cs
+++ Assets/AI.cs
@@ -6,34 +6,29 @@ public class AI : MonoBehaviour
 {
     private Selector tree;
 
-    private Rigidbody2D rb2d;
-    private Transform transform;
-
     public Transform[] patrolPoints;
 
     [SerializeField] private Sight sight;
-
     [SerializeField] private AIContext ai;
 
     private void Awake()
     {
-        rb2d      = GetComponent<Rigidbody2D>();
-        transform = GetComponent<Transform>();
-
-        ai = new AIContext(rb2d, transform, sight, patrolPoints);
+        ai = new AIContext(GetComponent<Rigidbody2D>(), 
+                           transform, 
+                           sight, 
+                           patrolPoints);
     }
 
     private void Start()
     {
         tree = new Selector(
+                     new Sequence(new TargetSighted(ai),
+                                  new SteerAtTarget(ai),
+                                  new ChaseTarget(ai)),
 
-            new Sequence(new TargetSighted(ai),
-                         new SteerAtTarget(ai),
-                         new ChaseTarget(ai)),
-
-            new Sequence(new FindDestination(ai),
-                         new SteerAtDestination(ai),
-                         new MoveToDestination(ai)));
+                     new Sequence(new FindDestination(ai),
+                                  new SteerAtDestination(ai),
+                                  new MoveToDestination(ai)));
     }
 
     private void FixedUpdate()
