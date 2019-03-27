f229d41d88689fbfd2c22f0ae0ee6c02c3cad8a9
Duy <etn218n@gmail.com>
2019-03-27 03:29:52 +0200

    Add Victim Spawner

---
+++ Assets/Spawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject victim;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            spawnPosition.z = 0f;
+
+            GameObject.Instantiate(victim, spawnPosition, Quaternion.identity, null);
+        }
+    }
+}
