71bc0c57b35e03ea415daab206041c553b9279a4
Duy <etn218n@gmail.com>
2019-03-30 13:45:20 +0200

    Set up Testting framework

---
+++ Assets/Scripts/BehaviourTree/Succeeder.cs
@@ -0,0 +1,16 @@
+﻿public class Succeeder : INode
+{
+    private INode child;
+
+    public Succeeder(INode child) { this.child = child; }
+
+    public NodeStatus Tick()
+    {
+        NodeStatus status = child.Tick();
+
+        if (status == NodeStatus.Running)
+            return NodeStatus.Running;
+
+        return NodeStatus.Sucess;
+    }
+}
